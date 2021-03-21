821308416766a416190bcae63e44d2af5ef74074
Shay.Asanova <shay.asanova@gmail.com>
2021-03-21 15:02:17 +0400

    Initial commit

---
+++ date.js
@@ -0,0 +1,25 @@
+// //This module that has a functionthat calls for a new date which we will export in app.js
+
+// exports.getDate = function () {
+
+//     const today = new Date();
+//     const options = {
+//       weekday: "short",
+//       day: "numeric",
+//       month: "long",
+//     };
+  
+//     return today.toLocaleDateString("en-US", options);
+  
+//   };
+  
+// exports.getDay = function () {
+//     const today = new Date();
+//     const options = {
+//         weekday: "short",
+  
+//     };
+  
+//     return today.toLocaleDateString("en-US", options);
+      
+// };
